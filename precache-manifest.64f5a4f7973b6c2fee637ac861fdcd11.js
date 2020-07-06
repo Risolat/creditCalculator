d9fe24a60a4b553b70f71cde80a3131c5e6f9881
Risolat <utkurova.risolat@mail.ru>
2020-07-06 18:19:48 +0500

    Updates

---
+++ precache-manifest.64f5a4f7973b6c2fee637ac861fdcd11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40897c5b63a2b7ce1ae3e32d623630b",
+    "url": "/creditCalculator/index.html"
+  },
+  {
+    "revision": "fdfd8976cdf45bfefe83",
+    "url": "/creditCalculator/static/css/2.60eec0d8.chunk.css"
+  },
+  {
+    "revision": "5a867b19e6b62b7ea93b",
+    "url": "/creditCalculator/static/css/main.5b9a1cb7.chunk.css"
+  },
+  {
+    "revision": "fdfd8976cdf45bfefe83",
+    "url": "/creditCalculator/static/js/2.2dfc96ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/creditCalculator/static/js/2.2dfc96ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a867b19e6b62b7ea93b",
+    "url": "/creditCalculator/static/js/main.b9f90fe1.chunk.js"
+  },
+  {
+    "revision": "cc2940124553428a3fd8",
+    "url": "/creditCalculator/static/js/runtime-main.95b0d878.js"
+  }
+]);
